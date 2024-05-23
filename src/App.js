77c5f8d3115af433db6cd3c49af47e5afa589039
Mr-aftab-khan <aftabahmad8704@gmail.com>
2024-05-24 00:03:25 +0500

    hero added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import Footer from "./Components/footer";
 import NavBar from "./Components/navBar";
+import Herosection from "./Components/herosection";
 function App() {
   return (
     <div className="App">
     {/* <NavBar/> */}
     <HomePage/>
     <Footer/>
+    <Herosection/>
     </div>
   );
 }
